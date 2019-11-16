5ff818d0e6bb19da3628f50e1786f054e71330e5
alertjjm <sdr2936@gmail.com>
2019-11-16 18:09:33 +0900

    191116

---
+++ SamplePageSliding/app/src/main/java/com/example/samplepagesliding/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.samplepagesliding;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+    boolean isPageOpen=false;
+    Animation translateLeftAnim;
+    Animation translateRightAnim;
+    LinearLayout page;
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        page=(LinearLayout)findViewById(R.id.page);
+        button=(Button) findViewById(R.id.button);
+        translateLeftAnim=AnimationUtils.loadAnimation(this,R.anim.translate_left);
+        translateRightAnim=AnimationUtils.loadAnimation(this,R.anim.translate_right);
+        SlidingPageAnimationListener animListener=new SlidingPageAnimationListener();
+        translateRightAnim.setAnimationListener(animListener);
+        translateLeftAnim.setAnimationListener(animListener);
+    }
+    private class SlidingPageAnimationListener implements Animation.AnimationListener{
+        @Override
+        public void onAnimationEnd(Animation animation) {
+            if(isPageOpen){
+                page.setVisibility(View.INVISIBLE);
+                button.setText("Open");
+                isPageOpen=false;
+            }else{
+                button.setText("Close");
+                isPageOpen=true;
+            }
+        }
+
+        @Override
+        public void onAnimationStart(Animation animation) {
+
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
+    }
+    public void onButton1clicked(View v){
+        if(isPageOpen){
+            page.startAnimation(translateRightAnim);
+        }else{
+            page.setVisibility(View.VISIBLE);
+            page.startAnimation(translateLeftAnim);
+        }
+    }
+}
