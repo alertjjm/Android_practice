0553e776a0a8924c178bbdffb1157729919fef3f
alertjjm <sdr2936@gmail.com>
2019-12-31 17:27:30 +0900

    191231

--- SampleLocationmap2/app/src/main/java/com/example/samplelocationmap/MainActivity.java
+++ SampleLocationmap2/app/src/main/java/com/example/samplelocationmap/MainActivity.java
@@ -1,7 +1,6 @@
-package com.example.samplelocation;
+package com.example.samplelocationmap;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.Context;
@@ -12,41 +11,46 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG="MainActivity";
+    SupportMapFragment mapFragment;
+    GoogleMap map;
+    Button button;
     Double latitude;
     Double longitude;
-    Button button;
-    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button=(Button)findViewById(R.id.button);
-        textView=(TextView)findViewById(R.id.textView);
         checkPermission();
-        button.setOnClickListener(new View.OnClickListener() {
+        mapFragment=(SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
-            public void onClick(View v) {
-                startLocationService();
-                textView.setText("내 위치 : Latitude: "+latitude+" Longitude: "+longitude);
+            public void onMapReady(GoogleMap googleMap) {
+                map=googleMap;
             }
         });
-    }
+        try{
+            MapsInitializer.initialize(this);
+        }catch (Exception e){
+        }
+        button=(Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                requestMyLocation();
+            }
+        });
 
-    private void startLocationService() {
-        LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        checkPermission();
-        Location location = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        latitude=location.getLatitude();
-        longitude=location.getLongitude();
-        GPSListener gpsListener=new GPSListener();
-        long minTime=10000;
-        float minDistance=0;
-        manager.requestLocationUpdates(LocationManager.GPS_PROVIDER,minTime,minDistance,gpsListener);
-        Toast.makeText(getApplicationContext(),"Location Service Started",Toast.LENGTH_SHORT).show();
     }
     private void checkPermission() {
         if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
@@ -68,28 +72,37 @@ public class MainActivity extends AppCompatActivity {
         }else{
         }
     }
-
-    private class GPSListener implements LocationListener{
+    private class GPSListener implements LocationListener {
         @Override
         public void onLocationChanged(Location location) {
-            Double latitude=location.getLatitude();
-            Double longitude=location.getLongitude();
-            textView.setText("내 위치 : Latitude: "+latitude+" Longitude: "+longitude);
+            showCurrentLocation(location);
         }
-
         @Override
         public void onStatusChanged(String provider, int status, Bundle extras) {
-
         }
-
         @Override
         public void onProviderEnabled(String provider) {
-
         }
-
         @Override
         public void onProviderDisabled(String provider) {
-
         }
     }
+    private void requestMyLocation(){
+        LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        checkPermission();
+        Location location = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        latitude=location.getLatitude();
+        longitude=location.getLongitude();
+        GPSListener gpsListener=new GPSListener();
+        long minTime=10000;
+        float minDistance=0;
+        manager.requestLocationUpdates(LocationManager.GPS_PROVIDER,minTime,minDistance,gpsListener);
+    }
+
+    private void showCurrentLocation(Location location) {
+        LatLng curPoint = new LatLng(location.getLatitude(), location.getLongitude());
+
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(curPoint, 15));
+    }
+
 }
